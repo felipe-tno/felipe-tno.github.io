c50f8ccd8f454fbc27c049e94265ba0149c763f4
Felipe Tavares <81234086+felipe-tno@users.noreply.github.com>
2023-06-20 22:11:19 -0300

    Add files via upload

---
+++ saibamais.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    lerInfos()
+}
+
+let divDetalhes = document.getElementById('detalhes')
+let btvoltar = document.getElementById('voltar')
+
+btvoltar.addEventListener('click', ()=>{
+    window.location.href = '../'
+})
+
+
+function lerInfos(){
+    divDetalhes.innerHTML = `
+    <img src="${localStorage.getItem('@Imagem')}">
+
+
+
+    <h1>${localStorage.getItem('@Nome')}</h1>
+    <h2>${localStorage.getItem('@Posicao')}</h2>
+    <p>${localStorage.getItem('@Descricao')}</p>
+    <div id="div-informacoes">
+        <span class="informacoes">
+            <strong>Nome completo: </strong> 
+            <span> ${localStorage.getItem('@Nome')} </span>
+        </span>
+    
+        <span class="informacoes">
+            <strong>Altura: </strong> 
+            <span> ${localStorage.getItem('@Altura')} </span>
+        </span>
+
+        <span class="informacoes">
+            <strong>Nascimento: </strong> 
+            <span> ${localStorage.getItem('@Nascimento')} 
+            </span>
+        </span>
+    </div>
+    `;
+}
